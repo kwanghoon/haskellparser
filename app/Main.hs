2dfd3d64cb6ff24480f55c9f5f8196a320494a6e
Kwanghoon Choi <lazyswamp@gmail.com>
2021-06-21 00:51:10 +0900

    write comment

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,26 @@ import SrcLoc
 import StringBuffer
 import ToolSettings
 
+{-
+Given
+
+ do { x <- m; return x }
+
+this haskell lexer produces a list of
+tokens and line and column information.
+
+$ stack exec lexer-exe
+(1,1,1,3): ITdo
+(1,4,1,5): ITocurly
+(1,6,1,7): ITvarid "x"
+(1,8,1,10): ITlarrow NormalSyntax
+(1,11,1,12): ITvarid "m"
+(1,12,1,13): ITsemi
+(1,14,1,20): ITvarid "return"
+(1,21,1,22): ITvarid "x"
+(1,23,1,24): ITccurly
+-}
+
 --
 main :: IO ()
 main = do
